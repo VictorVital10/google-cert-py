ac4276c3c3ef3a6b12c4fbd4b76a5c828fd40047
Victor Vital <vitassa2003@gmail.com>
2025-05-05 20:44:55 -0300

    sending codes

---
+++ index.py
@@ -0,0 +1,21 @@
+# call a variable
+device_id_1 = "01234"
+device_id_2 = "56789"
+print(device_id_2)
+
+# use type function
+device_id_3 = "h2b3n4"
+data_type = type(device_id_3)
+print(data_type) 
+
+
+# reassing a variable
+device_id_5 = "u38me0"
+print(device_id_5)
+device_id_5 = "h4"
+print(device_id_5)
+
+# demonstrate a typer error
+device_id_4 = "g6m3o0"
+number = 3
+print(device_id_4 + number)
