2df4900c96c2136658d7216ada2b9125841f887b
Victor Vital <vitassa2003@gmail.com>
2025-05-06 22:24:20 -0300

    variáveis, condicionais e operadores

--- index.py
+++ index.py
@@ -18,4 +18,37 @@ print(device_id_5)
 # demonstrate a typer error
 device_id_4 = "g6m3o0"
 number = 3
-print(device_id_4 + number)
+try:
+    print(device_id_4 + number)
+except TypeError as e:
+    print("Ocorreu um erro:", e)
+
+# create a conditional
+operating_system = "OS 3"
+if operating_system == "OS 3":
+    print("Updates Needed")
+else:
+    print("No Updates Needed")
+
+# determinar se atualizações são necessárias
+system = "OS 3"
+if system == "OS 3":
+    print("no update needed")
+elif system == "OS 1" or "OS 2":
+    print("update needed")     
+    
+# verificar se o usuário tem acesso ao dispositivo
+approved_user1 = "ederson"
+approved_user2 = "alisson"
+username = "sergio"
+if username == approved_user1 or username == approved_user2:
+    print("This user has access to this device")
+else:
+    print("This user does not have access to this device")
+
+# tentavias de login feitas durante o horário comercial
+comercial_hours = True
+if comercial_hours == False:
+    print("Tentativa de login feita durante o horário comercial")
+else:
+    print("Tentativa de login feita fora do horário comercial")
